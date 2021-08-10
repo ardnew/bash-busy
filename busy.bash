9463fa5cbc4de5edd53dabaa1779ab25b6fbbf97
ardnew <andrew@ardnew.com>
2021-08-10 12:03:27 -0500

    Update busy.bash
    Add better comments, replace `clear` screen with string overwrite

--- busy.bash
+++ busy.bash
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 busy() {
+	# Define the animation frames in-order.
 	#local -a frame=( '/' '-' '\' '|' )
 	local -a frame=( ⠋ ⠙ ⠹ ⠸ ⠼ ⠴ ⠦ ⠧ ⠇ ⠏ )
-	local delay='0.1s' # animated frame duration
-	local prompt       # message displayed after frame animation
+	
+	local delay='0.1s' # Animated frame duration
+	local prompt       # Message displayed after frame animation
 
-	# test if first argument appears to be a sleep(1) duration
-	pat='^([0-9](\.[0-9]*)?|[0-9]*\.[0-9]{1,})[smhd]?$'
+	# Test if first argument appears to be a sleep(1) duration.
+	local pat='^([0-9](\.[0-9]*)?|[0-9]*\.[0-9]{1,})[smhd]?$'
 	if [[ ${#} -gt 0 ]] && [[ "${1}" =~ ${pat} ]]; then
 		delay=${1}
 		prompt=${2}
@@ -17,13 +19,20 @@ busy() {
 		prompt=${1}
 		shift
 	fi
+	
+	# Find the widest frame in the animation.
+	local wide=0
+	for s in "${frame[@]}"; do 
+		[[ ${#s} -gt ${wide} ]] && wide=${#s}
+	done
 
-	clear
-	n=0
-	printf "   %s\r" "${prompt}"
+	local n=0
+	printf ' %*s %s\r' ${wide} '' "${prompt}"
 	while ! eval -- "${*}"; do
-		printf " %s \r" "${frame[((n=++n%${#frame[@]}))]}"
+		printf ' %*s \r' ${wide} "${frame[((n=++n%${#frame[@]}))]}"
 		sleep "${delay}"
 	done
+	# Clear the animation frame and prompt
+	printf ' %*s %*s\r' ${wide} '' ${#prompt} ''
 }
 
